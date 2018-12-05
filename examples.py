8d15d7e02bfdabf04fcf4d024847a77d734167b0
git admin <ishtikhar.khan@pay1.in>
2018-12-05 22:34:48 +0530

    modified example.py

---
+++ examples.py
@@ -0,0 +1,10 @@
+example=input("are you testing git")
+if testing=='yes' and checking=='yo'
+   print(" yes i am testing git ")
+   print(" pizza ")
+   print(" burger ")
+   print(" fries ")
+else
+  print(" no i am not testing git") 
+  print(" no i am also testing github") 
+  print(" no i am also testing github.com") 
