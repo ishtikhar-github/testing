810ded7b1f1e66d6d1f0dc11739309e7196cf2c3
vibhasmindsarray <pay1.master@mindsarray.com>
2019-09-23 18:20:55 +0530

    edited script

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ git status
 git add . 
 read -p "Added New File:" description
 git commit -m "$description" 
-read -p "Enter Branch to push codes:" branch
+read -p "Enter Branch to push the codes:" branch
 git push origin "$branch"
 
